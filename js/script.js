3acb37b9fc8c2639a01f19dd54454f413af90eee
jjedsone <138830314+jjedsone@users.noreply.github.com>
2024-03-19 10:36:03 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ ScrollReveal().reveal(".home-content h1,.about-img", { origin: "left" });
 ScrollReveal().reveal(".home-content p,.about-content", { origin: "right" });
 
 const typed = new Typed(".multiple-text", {
-  strings: ["Desenvolverdor HTML", "CSS Java script", "Python"],
+  strings: ["Desenvolvedor HTML", "CSS Java script", "Python"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
